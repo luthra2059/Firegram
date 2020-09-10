66d128e8657748746c19ede091a4a3238ea1ae44
Shubham Luthra <luthra2059@gmail.com>
2020-09-10 17:33:18 +0530

    Firebase Configured

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDqGApY1k5OIvKvsZAsnvhpG9Sr5S3UAjQ",
+    authDomain: "firegram-98361.firebaseapp.com",
+    databaseURL: "https://firegram-98361.firebaseio.com",
+    projectId: "firegram-98361",
+    storageBucket: "firegram-98361.appspot.com",
+    messagingSenderId: "240711358844",
+    appId: "1:240711358844:web:1bfa73b2781738567b2cf9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+
+  export {projectFirestore, projectStorage};
