4695266fde75c10b3517b3973d160f1ea6d297e8
Shubham Luthra <48431503+luthra2059@users.noreply.github.com>
2020-09-11 08:50:54 +0530

    Removed API keys for security reasons
    Add your own Firebase API keys for the project to run

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDqGApY1k5OIvKvsZAsnvhpG9Sr5S3UAjQ",
-    authDomain: "firegram-98361.firebaseapp.com",
-    databaseURL: "https://firegram-98361.firebaseio.com",
-    projectId: "firegram-98361",
-    storageBucket: "firegram-98361.appspot.com",
-    messagingSenderId: "240711358844",
-    appId: "1:240711358844:web:1bfa73b2781738567b2cf9"
+    apiKey: "Your api key",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
